b7a0ec078db8cb1bbccd7ae63af34631211fd07a
jinkwan <kkole3897@gmail.com>
2024-03-04 00:15:21 +0900

    feat: Gallery 컴포넌트 추가

---
+++ app/game/[gameId]/components/gallery/gallery.css.ts
@@ -0,0 +1,65 @@
+import { style, styleVariants } from '@vanilla-extract/css';
+
+export const gallery = style({
+  position: 'relative',
+  overflow: 'hidden',
+});
+
+export const galleryView = style({
+  display: 'flex',
+  transitionProperty: 'transform',
+  touchAction: 'pan-y',
+  userSelect: 'none',
+  transition: 'transform 0.2s',
+});
+
+const controlButtonBase = style({
+  display: 'none',
+  alignItems: 'center',
+  justifyContent: 'center',
+  position: 'absolute',
+  top: '0',
+  bottom: '0',
+  width: '32px',
+  padding: 0,
+  border: 0,
+  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  color: '#fff',
+  cursor: 'pointer',
+  ':disabled': {
+    cursor: 'default',
+    opacity: 0.45,
+  },
+  selectors: {
+    [`${gallery}:hover &`]: {
+      display: 'flex',
+    },
+  },
+});
+
+export const controlButton = styleVariants({
+  prev: [
+    controlButtonBase,
+    {
+      left: 0,
+    },
+  ],
+  next: [
+    controlButtonBase,
+    {
+      right: 0,
+    },
+  ],
+});
+
+export const slide = style({
+  position: 'relative',
+  flex: '0 0 100%',
+  backgroundColor: 'blue',
+  aspectRatio: '16 / 9',
+});
+
+export const image = style({
+  objectFit: 'cover',
+  pointerEvents: 'none',
+});
---
+++ app/game/[gameId]/components/gallery/gallery.tsx
@@ -0,0 +1,136 @@
+'use client';
+
+import Image from 'next/image';
+import {
+  useState,
+  useRef,
+  useEffect,
+  useMemo,
+  useCallback,
+  PointerEventHandler,
+} from 'react';
+import { RiArrowRightSLine, RiArrowLeftSLine } from '@remixicon/react';
+
+import * as Carousel from '@/components/carousel';
+import * as styles from './gallery.css';
+
+type GalleryProps = {
+  contents: { url: string; name?: string }[];
+  name: string;
+};
+
+export default function Gallery(props: GalleryProps) {
+  const { contents } = props;
+
+  const [slideIndex, setSlideIndex] = useState(0);
+  const carouselRef = useRef<HTMLDivElement>(null);
+  const [carouselWidth, setCarouselWidth] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+  const [mouseDX, setMouseDX] = useState(0);
+
+  useEffect(() => {
+    if (carouselRef.current) {
+      const observer = new ResizeObserver(([entry]) => {
+        const [borderContentSize] = entry.borderBoxSize;
+        setCarouselWidth(borderContentSize.inlineSize);
+      });
+
+      observer.observe(carouselRef.current);
+
+      return () => {
+        observer.disconnect();
+      };
+    }
+  }, []);
+
+  const onSlideNext = useCallback(() => {
+    if (slideIndex === contents.length - 1) {
+      return;
+    }
+
+    setSlideIndex(slideIndex + 1);
+  }, [slideIndex, contents.length]);
+
+  const onSlidePrev = useCallback(() => {
+    if (slideIndex === 0) {
+      return;
+    }
+
+    setSlideIndex(slideIndex - 1);
+  }, [slideIndex]);
+
+  const onPointerDown: PointerEventHandler<HTMLDivElement> = useCallback(
+    (downEvent) => {
+      setIsDragging(true);
+
+      const onPointerMove = (moveEvent: PointerEvent) => {
+        setMouseDX(moveEvent.screenX - downEvent.screenX);
+      };
+
+      const onPointerUp = (upEvent: PointerEvent) => {
+        const dx = upEvent.screenX - downEvent.screenX;
+        if (dx < -50) {
+          onSlideNext();
+        } else if (dx > 50) {
+          onSlidePrev();
+        }
+        setIsDragging(false);
+        setMouseDX(0);
+        document.removeEventListener('pointermove', onPointerMove);
+      };
+
+      document.addEventListener('pointermove', onPointerMove);
+      document.addEventListener('pointerup', onPointerUp, { once: true });
+    },
+    [onSlideNext, onSlidePrev]
+  );
+
+  const viewStyle = useMemo(() => {
+    if (isDragging) {
+      return {
+        transform: `translateX(${-1 * carouselWidth * slideIndex + mouseDX}px)`,
+        transition: 'none',
+      };
+    }
+
+    return {
+      transform: `translateX(${-1 * carouselWidth * slideIndex}px)`,
+    };
+  }, [slideIndex, carouselWidth, isDragging, mouseDX]);
+
+  return (
+    <Carousel.Root
+      value={slideIndex}
+      className={styles.gallery}
+      ref={carouselRef}
+      onPointerDown={onPointerDown}
+    >
+      <Carousel.View className={styles.galleryView} style={viewStyle}>
+        {contents.map((content) => {
+          return (
+            <Carousel.Slide key={content.url} className={styles.slide}>
+              <Image
+                src={content.url}
+                alt={content.name ?? props.name}
+                fill
+                className={styles.image}
+              />
+            </Carousel.Slide>
+          );
+        })}
+      </Carousel.View>
+      <Carousel.Prev
+        className={styles.controlButton.prev}
+        onClick={onSlidePrev}
+      >
+        <RiArrowLeftSLine />
+      </Carousel.Prev>
+      <Carousel.Next
+        className={styles.controlButton.next}
+        onClick={onSlideNext}
+      >
+        <RiArrowRightSLine />
+      </Carousel.Next>
+    </Carousel.Root>
+  );
+}
