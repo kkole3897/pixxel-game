b8e29c31a8357a3a1bcb73f5aecccfac66312200
jinkwan <kkole3897@gmail.com>
2024-05-12 15:07:14 +0900

    feat: 닉네임 중복 체크 api 호출 함수 추가

---
+++ src/shared/api/core/users.ts
@@ -0,0 +1,26 @@
+import { coreApiUrl } from '@/shared/config';
+
+type CheckNicknameResponse = {
+  isValid: boolean;
+};
+
+export async function checkNickname(
+  nickname: string
+): Promise<CheckNicknameResponse> {
+  const uri = `${coreApiUrl}/users/check/nickname?${nickname}`;
+
+  const response = await fetch(uri, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    // TODO: checkNickname api call error 구체화
+    throw new Error();
+  }
+
+  const data = await response.json();
+
+  return data;
+}
