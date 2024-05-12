09bcc13917d2b38b1ea80f04ec64e3c69e982392
jinkwan <kkole3897@gmail.com>
2024-05-12 12:37:03 +0900

    feat: UrlSearchParams plain object 변환 함수 추가

--- src/shared/lib/object.ts
+++ src/shared/lib/object.ts
@@ -1,3 +1,15 @@
 export function toPlainObject(complexObj: object) {
   return JSON.parse(JSON.stringify(complexObj));
 }
+
+export function toURLSearchParamsObject(searchParams: URLSearchParams) {
+  const obj = Array.from(searchParams.entries()).reduce<{
+    [key: string]: string;
+  }>((acc, [key, value]) => {
+    acc[key] = value;
+
+    return acc;
+  }, {});
+
+  return obj;
+}
