243b7927e73c60816c61c1af7d1ee454fd97fd77
jinkwan <kkole3897@gmail.com>
2024-05-14 15:32:13 +0900

    feat: shared/ui/check-line 추가

---
+++ src/shared/ui/icons/check-line/check-line.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import CheckLine from './check-line';
+
+const meta: Meta<typeof CheckLine> = {
+  title: 'Shared/Icons/CheckLine',
+  component: CheckLine,
+  tags: ['autodocs'],
+  argTypes: {
+    color: {
+      control: 'color',
+      description: '아이콘 색상',
+      table: {
+        defaultValue: {
+          summary: '#000000',
+        },
+      },
+    },
+    size: {
+      control: 'number',
+      description: '아이콘 크기, 정방형으로 조절 가능',
+      table: {
+        defaultValue: {
+          summary: 24,
+        },
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof CheckLine>;
+
+export const Default: Story = {};
---
+++ src/shared/ui/icons/check-line/index.ts
@@ -0,0 +1,1 @@
+export { default as CheckLine } from './check-line';
