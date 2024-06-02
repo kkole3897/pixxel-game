9eed73fca30375a573da3ff25c3ec8c817f6701a
jinkwan <kkole3897@gmail.com>
2024-06-02 23:44:49 +0900

    feat(price-history-chart2): tooltip 기본 구조 추가

--- src/entities/game/ui/price-history-chart-2/price-history-chart-2.tsx
+++ src/entities/game/ui/price-history-chart-2/price-history-chart-2.tsx
@@ -6,11 +6,16 @@ import { Brush } from '@visx/brush';
 import { Group } from '@visx/group';
 import { useParentSize } from '@visx/responsive';
 import { AxisBottom, AxisRight } from '@visx/axis';
-import { max, extent, min } from '@visx/vendor/d3-array';
-import { curveStep } from '@visx/curve';
+import { max, extent, min, bisector } from '@visx/vendor/d3-array';
+import { curveStepAfter } from '@visx/curve';
 import { LinePath } from '@visx/shape';
 import { Bounds } from '@visx/brush/lib/types';
 import BaseBrush from '@visx/brush/lib/BaseBrush';
+import { localPoint } from '@visx/event';
+import { useTooltip, Tooltip } from '@visx/tooltip';
+import dayjs from 'dayjs';
+
+import * as styles from './price-history-chart-2.css';
 
 type PriceHistoryRecord = {
   startAt: string;
@@ -22,6 +27,7 @@ const series: PriceHistoryRecord[] = [
   { startAt: '2024-05-03', price: 9000 },
   { startAt: '2024-05-07', price: 7000 },
   { startAt: '2024-06-01', price: 10000 },
+  { startAt: '2024-06-02', price: 10000 },
 ];
 
 function getDate(record: PriceHistoryRecord) {
@@ -31,6 +37,10 @@ function getDate(record: PriceHistoryRecord) {
 function getValue(record: PriceHistoryRecord) {
   return record.price;
 }
+
+const bisectDate = bisector<PriceHistoryRecord, Date>(
+  (d) => new Date(d.startAt)
+).left;
 
 export default function PriceHistoryChart2() {
   const initialWidth = 200;
@@ -62,23 +72,19 @@ export default function PriceHistoryChart2() {
   const xBrushMax = Math.max(width - brushMargin.left - brushMargin.right, 0);
   const yBrushMax = Math.max(brushHeight, 0);
 
-  const [dateRange, setDateRange] = useState([
-    getDate(series[0]),
-    getDate(series[series.length - 1]),
-  ]);
+  const [filteredSeries, setFilterSeries] = useState(series);
 
   const dateScale = useMemo(
     () =>
       scaleTime({
-        domain: dateRange,
+        domain: extent(filteredSeries, getDate) as [Date, Date],
         range: [0, xMax],
-        clamp: true,
       }),
-    [dateRange, xMax]
+    [filteredSeries, xMax]
   );
 
-  const minPrice = Math.max((min(series, getValue) || 0) - 1000, 0);
-  const maxPrice = max(series, getValue) || 0;
+  const minPrice = 0;
+  const maxPrice = (max(series, getValue) || 0) * 2;
 
   const priceScale = useMemo(
     () =>
@@ -121,19 +127,95 @@ export default function PriceHistoryChart2() {
     }
 
     const { x0, x1 } = domain;
-    setDateRange([new Date(x0), new Date(x1)]);
+    const startDate = new Date(x0);
+    const endDate = new Date(x1);
+    let seriesCopy = series.filter((s) => {
+      const date = getDate(s);
+
+      return startDate <= date && date <= endDate;
+    });
+    const firstIndex = series.findLastIndex((s) => getDate(s) < startDate);
+    seriesCopy = [
+      {
+        startAt: startDate.toISOString(),
+        price: series[Math.max(firstIndex, 0)].price,
+      },
+      ...seriesCopy,
+    ];
+    const lastIndex = series.findLastIndex((s) => getDate(s) < endDate);
+    seriesCopy = [
+      ...seriesCopy,
+      {
+        startAt: endDate.toISOString(),
+        price: series[lastIndex === -1 ? series.length - 1 : lastIndex].price,
+      },
+    ];
+    setFilterSeries(seriesCopy);
+  };
+
+  const {
+    tooltipTop,
+    tooltipLeft,
+    tooltipOpen,
+    hideTooltip,
+    showTooltip,
+    tooltipData = { date: '2024.06.02', price: '0원' },
+  } = useTooltip<{ date: string; price: string }>();
+
+  const handleTooltip = (
+    event: React.TouchEvent<SVGRectElement> | React.MouseEvent<SVGRectElement>
+  ) => {
+    const { x } = localPoint(event) || { x: 0 };
+    const x0 = dateScale.invert(x);
+    const index = bisectDate(series, x0, 1);
+    const d0 = series[index - 1];
+    const d1 = series[index];
+    let d = d0;
+    if (d1 && getDate(d1)) {
+      d =
+        x0.valueOf() - getDate(d0).valueOf() >
+        getDate(d1).valueOf() - x0.valueOf()
+          ? d1
+          : d0;
+    }
+
+    if (
+      d.startAt < filteredSeries[0].startAt ||
+      filteredSeries[filteredSeries.length - 1].startAt < d.startAt
+    ) {
+      return;
+    }
+
+    const tooltipPos = {
+      tooltipData: {
+        date: dayjs(getDate(d)).format('YYYY.MM.DD'),
+        price: `${getValue(d).toLocaleString()}원`,
+      },
+      tooltipLeft: dateScale(getDate(d)),
+      tooltipTop: priceScale(getValue(d)),
+    };
+    showTooltip(tooltipPos);
   };
 
   return (
-    <div ref={parentRef}>
+    <div ref={parentRef} className={styles.container}>
       <svg width={width} height={height}>
         <Group left={margin.left} top={margin.top}>
           <LinePath
-            curve={curveStep}
-            data={series}
+            curve={curveStepAfter}
+            data={filteredSeries}
             x={(d) => dateScale(getDate(d))}
             y={(d) => priceScale(getValue(d))}
             stroke="#000"
+          />
+          <rect
+            width={width - margin.left - margin.right}
+            height={topChartHeight}
+            fill="transparent"
+            onTouchMove={handleTooltip}
+            onTouchStart={handleTooltip}
+            onMouseMove={handleTooltip}
+            onMouseLeave={() => hideTooltip()}
           />
           <AxisBottom
             scale={dateScale}
@@ -151,7 +233,7 @@ export default function PriceHistoryChart2() {
         </Group>
         <Group left={margin.left} top={margin.top + topChartHeight + rowGap}>
           <LinePath
-            curve={curveStep}
+            curve={curveStepAfter}
             data={series}
             x={(d) => brushDateScale(getDate(d))}
             y={(d) => brushPriceScale(getValue(d))}
@@ -168,16 +250,17 @@ export default function PriceHistoryChart2() {
             brushDirection="horizontal"
             initialBrushPosition={initialBrushPosition}
             onChange={onBrushChange}
-            onClick={() =>
-              setDateRange([
-                new Date(series[0].startAt),
-                new Date(series[series.length - 1].startAt),
-              ])
-            }
+            onClick={() => setFilterSeries(series)}
             useWindowMoveEvents
           />
         </Group>
       </svg>
+      {tooltipOpen && (
+        <Tooltip top={tooltipTop} left={tooltipLeft}>
+          <div>날짜: {tooltipData.date}</div>
+          <div>가격: {tooltipData.price}</div>
+        </Tooltip>
+      )}
     </div>
   );
 }
