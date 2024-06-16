e07c7b07bb2b91b568a9adffb2c24c2598c1728d
jinkwan <kkole3897@gmail.com>
2024-06-16 17:51:20 +0900

    fix: storybook table summary type 오류 수정
    - storybook 8로 업데이트하면서 type 변경되어 적용

--- src/shared/ui/icons/check-line/check-line.stories.tsx
+++ src/shared/ui/icons/check-line/check-line.stories.tsx
@@ -21,7 +21,7 @@ const meta: Meta<typeof CheckLine> = {
       description: '아이콘 크기, 정방형으로 조절 가능',
       table: {
         defaultValue: {
-          summary: 24,
+          summary: '24',
         },
       },
     },
