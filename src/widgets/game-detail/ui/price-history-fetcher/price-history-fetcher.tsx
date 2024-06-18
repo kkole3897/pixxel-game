9062dc60b0e7639b9f4e5f844c78cc28e9aeece6
jinkwan <kkole3897@gmail.com>
2024-06-18 20:40:19 +0900

    feat: 로딩, 에러 layout shift 발생하지 않도록 간단 적용

--- src/widgets/game-detail/ui/price-history-fetcher/price-history-fetcher.tsx
+++ src/widgets/game-detail/ui/price-history-fetcher/price-history-fetcher.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import cn from 'classnames';
+
 import {
   useGetBestGamePriceHistoryQuery,
   GamePriceHistoryChart,
 } from '@/entities/game';
+import * as styles from './price-history-fetcher.css';
 
 interface PriceHistoryFetcherProps {
   className?: string;
@@ -17,16 +20,19 @@ export default function PriceHistoryContainer({
   const { isPending, data, isError } =
     useGetBestGamePriceHistoryQuery(gamePublicId);
 
+  const composedRootClassName = cn(className, styles.container);
+
+  // TODO: 로딩, 에러 처리 구체화
   if (isPending) {
-    return <div className={className}>로딩 중...</div>;
+    return <div className={composedRootClassName}></div>;
   }
 
   if (isError) {
-    return <div className={className}>에러</div>;
+    return <div className={composedRootClassName}></div>;
   }
 
   return (
-    <div className={className}>
+    <div className={composedRootClassName}>
       <GamePriceHistoryChart data={data} />
     </div>
   );
