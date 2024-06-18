a688ed30bd78e3d7768398bf6cc3461231128937
jinkwan <kkole3897@gmail.com>
2024-06-19 01:09:16 +0900

    feat(WishButton): 로그인하지 않은 경우 로그인 페이지로 이동 추가

--- src/features/toggle-wish/ui/wish-button/wish-button.tsx
+++ src/features/toggle-wish/ui/wish-button/wish-button.tsx
@@ -1,20 +1,34 @@
 'use client';
 
 import { RiHeartFill } from '@remixicon/react';
+import { useRouter } from 'next/navigation';
 
 import { useToggleWish } from '../../lib';
 import * as styles from './wish-button.css';
 import { revalidateWishlist } from './action';
+import { createClient } from '@/shared/lib/supabase/client';
 
 type WishButtonProps = {
   gameId: string;
 };
 
 export default function WishButton(props: WishButtonProps) {
+  const supabase = createClient();
+  const router = useRouter();
+
   const { isWished, toggle } = useToggleWish(props.gameId);
   const heartColor = isWished ? '#fff' : 'rgba(255, 255, 255, 0.5)';
 
   const handleClick = async () => {
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session === null) {
+      router.push('/login');
+      return;
+    }
+
     await toggle();
     revalidateWishlist();
   };
