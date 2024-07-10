5628531b37e8f5c44748e93e7e182b4b651ca48b
jinkwan <kkole3897@gmail.com>
2024-07-10 15:36:51 +0900

    feat: 이용약관 간단 표시 추가

--- src/app/login/page.css.ts
+++ src/app/login/page.css.ts
@@ -1,5 +1,7 @@
 import { style } from '@vanilla-extract/css';
 
+import { theme } from '@/shared/styles/theme.css';
+
 export const rootContainer = style({
   display: 'flex',
   flexDirection: 'column',
@@ -19,4 +21,21 @@ export const socialLoginArea = style({
   flexDirection: 'column',
   rowGap: '8px',
   marginTop: '32px',
+});
+
+export const description = style({
+  color: theme.colors.gray[800],
+  fontWeight: 500,
+});
+
+export const terms = style({
+  marginTop: '32px',
+  color: theme.colors.gray[500],
+  textAlign: 'center',
+});
+
+export const termsStrongText = style({
+  color: 'inherit',
+  fontWeight: 'inherit',
+  textDecoration: 'underline',
 });
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,9 +17,21 @@ export default function LoginPage() {
   return (
     <div className={styles.rootContainer}>
       <div className={styles.logoArea}>Pixxel Game</div>
-      <div>소셜 로그인으로 간편하게 시작하세요</div>
+      <div className={styles.description}>
+        소셜 로그인으로 간편하게 시작하세요
+      </div>
       <div className={styles.socialLoginArea}>
         <GoogleLoginButton />
+      </div>
+      <div className={styles.terms}>
+        로그인 시{' '}
+        <strong className={styles.termsStrongText}>서비스 이용약관</strong>과{' '}
+        <strong className={styles.termsStrongText}>
+          개인정보 수집 및 이용
+        </strong>
+        에 동의하게 되며,
+        <br />
+        서비스 이용을 위해 이메일, 이름, 프로필 이미지를 수집합니다.
       </div>
     </div>
   );
