948b04e402e67fb9dd554b916cdd356710280d7d
jinkwan <kkole3897@gmail.com>
2024-07-26 18:53:31 +0900

    chore: google redirect url 디버그

--- src/features/auth/by-google/model/login-by-google.ts
+++ src/features/auth/by-google/model/login-by-google.ts
@@ -4,6 +4,8 @@ import { google } from '@/shared/config';
 export async function loginByGoogle() {
   const supabase = createBrowserClient();
 
+  console.log(google.redirectUri);
+
   await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
