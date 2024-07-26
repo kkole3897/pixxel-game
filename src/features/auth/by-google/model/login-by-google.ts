fa74e4be90161408e27b97230af5d2ddf2340f55
jinkwan <kkole3897@gmail.com>
2024-07-26 19:57:31 +0900

    Revert "chore: google redirect url 디버그"
    This reverts commit 948b04e402e67fb9dd554b916cdd356710280d7d.

--- src/features/auth/by-google/model/login-by-google.ts
+++ src/features/auth/by-google/model/login-by-google.ts
@@ -4,8 +4,6 @@ import { google } from '@/shared/config';
 export async function loginByGoogle() {
   const supabase = createBrowserClient();
 
-  console.log(google.redirectUri);
-
   await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
