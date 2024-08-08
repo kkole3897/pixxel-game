94d71b470fe8f60f4a92d46dde79bed33c1c537c
jinkwan <kkole3897@gmail.com>
2024-08-08 18:52:11 +0900

    chore: entities/session 삭제
    - supabase migration 이전 불필요한 코드

--- src/entities/session/constants/index.ts
+++
@@ -1,3 +0,0 @@
-export const ACCESS_TOKEN_KEY = 'accessToken';
-export const REFRESH_TOKEN_KEY = 'refreshToken';
-export const TEMP_TOKEN_KEY = 'tempToken';
--- src/entities/session/model/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './viewer';
-export * as ServerSession from './server-session';
-export * as TempServerSession from './temp-server-session';
