ead43f385d2477614b712179fae7ea85d21728d0
jinkwan <kkole3897@gmail.com>
2024-09-14 23:22:46 +0900

    feat(features/request-new-game/lib/clean-url): checkEpicUrl 추가

--- src/features/request-new-game/lib/clean-url.test.ts
+++ src/features/request-new-game/lib/clean-url.test.ts
@@ -1,4 +1,4 @@
-import { checkSteamUrl } from './clean-url';
+import { checkSteamUrl, checkEpicUrl } from './clean-url';
 
 describe('checkSteamUrl', () => {
   it('should return true for standard steam app url', () => {
@@ -46,5 +46,114 @@ describe('checkSteamUrl', () => {
     const unexpectedUrl =
       'https://store.steampowered.com/hello/1234567/My_Game';
     expect(checkSteamUrl(unexpectedUrl)).toBe(false);
+  });
+});
+
+describe('checkEpicUrl', () => {
+  it('should return true for standard epic url', () => {
+    const url = 'https://store.epicgames.com/p/my-game';
+
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for standard epic url with trailing slash', () => {
+    const url = 'https://store.epicgames.com/p/my-game/';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('shoud return true for epic url with query string', () => {
+    const url = `https://store.epicgames.com/p/my-game?utm_source=google&utm_medium=cpc&utm_campaign=shopping&gclid=Cj0KCQjwzYGGBhCTARIsANlgoO`;
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('shoud return true for ko locale epic url', () => {
+    const url = 'https://store.epicgames.com/ko/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for en-US locale epic url', () => {
+    const url = 'https://store.epicgames.com/en-US/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for ar locale epic url', () => {
+    const url = 'https://store.epicgames.com/ar/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for de locale epic url', () => {
+    const url = 'https://store.epicgames.com/de/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for es-ES locale epic url', () => {
+    const url = 'https://store.epicgames.com/es-ES/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for es-MX locale epic url', () => {
+    const url = 'https://store.epicgames.com/es-MX/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for fr locale epic url', () => {
+    const url = 'https://store.epicgames.com/fr/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for it locale epic url', () => {
+    const url = 'https://store.epicgames.com/it/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for ja locale epic url', () => {
+    const url = 'https://store.epicgames.com/ja/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for pl locale epic url', () => {
+    const url = 'https://store.epicgames.com/pl/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for pt-BR locale epic url', () => {
+    const url = 'https://store.epicgames.com/pt-BR/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for ru locale epic url', () => {
+    const url = 'https://store.epicgames.com/ru/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for th locale epic url', () => {
+    const url = 'https://store.epicgames.com/th/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for tr locale epic url', () => {
+    const url = 'https://store.epicgames.com/tr/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for zh-CN locale epic url', () => {
+    const url = 'https://store.epicgames.com/zh-CN/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return true for zh-Hant locale epic url', () => {
+    const url = 'https://store.epicgames.com/zh-Hant/p/my-game';
+    expect(checkEpicUrl(url)).toBe(true);
+  });
+
+  it('should return false for non-epic url', () => {
+    const nonEpicUrl = 'https://www.google.com';
+    expect(checkEpicUrl(nonEpicUrl)).toBe(false);
+  });
+
+  it('should return false when path is not started with p', () => {
+    const unexpectedUrl =
+      'https://store.epicgames.com/ko/browse?sortBy=releaseDate&sortDir=DESC&category=Game&count=40';
+    expect(checkEpicUrl(unexpectedUrl)).toBe(false);
   });
 });
--- src/features/request-new-game/lib/clean-url.ts
+++ src/features/request-new-game/lib/clean-url.ts
@@ -3,3 +3,9 @@ export function checkSteamUrl(url: string): boolean {
     /https:\/\/store.steampowered.com\/(app|bundle|sub)\/\d+(\/.+)?/;
   return regex.test(url);
 }
+
+export function checkEpicUrl(url: string): boolean {
+  const regex =
+    /https:\/\/store.epicgames.com(\/(ko|en-US|ar|de|es-ES|es-MX|fr|it|ja|pl|pt-BR|ru|th|tr|zh-CN|zh-Hant))?\/p(\/.+)?/;
+  return regex.test(url);
+}
