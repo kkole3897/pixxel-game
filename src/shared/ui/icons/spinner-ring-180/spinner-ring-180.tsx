cabde294332f08368a2c22bd29d207aec46e526d
jinkwan <kkole3897@gmail.com>
2024-09-26 17:08:33 +0900

    feat(shared/ui/icons): SpinnerRing180 forwardRef로 변경

--- src/shared/ui/icons/spinner-ring-180/spinner-ring-180.tsx
+++ src/shared/ui/icons/spinner-ring-180/spinner-ring-180.tsx
@@ -1,3 +1,4 @@
+import React, { forwardRef } from 'react';
 import { CommonSvgIconProps } from '../types';
 
 type SpinnerRing180Props = CommonSvgIconProps & {
@@ -11,29 +12,32 @@ type SpinnerRing180Props = CommonSvgIconProps & {
   color?: string;
 };
 
-export default function SpinnerRing180({
-  size = 24,
-  color = 'currentColor',
-  ...props
-}: SpinnerRing180Props) {
-  return (
-    <svg
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      fill={color}
-      {...props}
-    >
-      <path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z">
-        <animateTransform
-          attributeName="transform"
-          type="rotate"
-          dur="0.75s"
-          values="0 12 12;360 12 12"
-          repeatCount="indefinite"
-        />
-      </path>
-    </svg>
-  );
-}
+const SpinnerRing180 = forwardRef<SVGSVGElement, SpinnerRing180Props>(
+  ({ size = 24, color = 'currentColor', ...props }, forwardedRef) => {
+    return (
+      <svg
+        ref={forwardedRef}
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        fill={color}
+        {...props}
+      >
+        <path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z">
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            dur="0.75s"
+            values="0 12 12;360 12 12"
+            repeatCount="indefinite"
+          />
+        </path>
+      </svg>
+    );
+  }
+);
+
+SpinnerRing180.displayName = 'SpinnerRing180';
+
+export default SpinnerRing180;
