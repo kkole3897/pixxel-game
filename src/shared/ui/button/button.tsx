4e4634bb61ddb2031301fa1a59bd8fc2d7189046
jinkwan <kkole3897@gmail.com>
2024-09-26 18:09:05 +0900

    feat(shared/ui): LoadingButton 추가

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -8,7 +8,7 @@ import * as styles from './button.css';
 type ButtonVariant = 'solid' | 'ghost' | 'text';
 type Size = 'lg' | 'md';
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   /**
    * @defaultValue `solid`
    */
---
+++ src/shared/ui/loading-button/loading-button.tsx
@@ -0,0 +1,34 @@
+import { forwardRef } from 'react';
+
+import { Button, type ButtonProps } from '@/shared/ui/button';
+import { SpinnerRing1 } from '@/shared/ui/icons';
+
+type LoadingButtonProps = ButtonProps & {
+  isLoading?: boolean;
+  /**
+   * @default SpinnerRing1
+   */
+  spinner?: React.ReactNode;
+};
+
+const LoadingButton = forwardRef<HTMLButtonElement, LoadingButtonProps>(
+  (
+    { isLoading, children, disabled, spinner = <SpinnerRing1 />, ...props },
+    forwardedRef
+  ) => {
+    return (
+      <Button
+        ref={forwardedRef}
+        {...props}
+        disabled={disabled || isLoading}
+        data-loading={isLoading ? '' : undefined}
+      >
+        {isLoading ? spinner : children}
+      </Button>
+    );
+  }
+);
+
+LoadingButton.displayName = 'LoadingButton';
+
+export default LoadingButton;
