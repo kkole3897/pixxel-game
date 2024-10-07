f79b671c6135095c463de78f336606892b7dbf88
jinkwan <kkole3897@gmail.com>
2024-10-07 23:04:49 +0900

    feat(empty-search-result): 게임 요청 페이지 이동 링크 추가

--- src/widgets/search-game-fetcher/ui/search-game-fetcher/search-game-fetcher.css.ts
+++ src/widgets/search-game-fetcher/ui/search-game-fetcher/search-game-fetcher.css.ts
@@ -1,9 +1,9 @@
 import { style } from '@vanilla-extract/css';
 
 export const emptySearchResult = style({
-  padding: '24px',
+  padding: '96px 24px 24px',
 });
 
 export const searchGameError = style({
-  padding: '24px',
+  padding: '96px 24px 24px',
 });
