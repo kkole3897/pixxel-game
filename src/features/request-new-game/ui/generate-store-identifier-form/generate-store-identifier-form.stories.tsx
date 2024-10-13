879d996817e2a271a1673bf5266e25ac5abf7444
jinkwan <kkole3897@gmail.com>
2024-10-13 19:09:59 +0900

    fix(generate-store-identifier-form): storybook provider 설정 누락 수정

--- src/features/request-new-game/ui/generate-store-identifier-form/generate-store-identifier-form.stories.tsx
+++ src/features/request-new-game/ui/generate-store-identifier-form/generate-store-identifier-form.stories.tsx
@@ -1,11 +1,24 @@
 import { type Meta, StoryObj } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import GenerateStoreIdentifierForm from './generate-store-identifier-form';
+import { GeneratedStoreIdentifierStoreProvider } from '@/features/request-new-game';
+
+const queryClient = new QueryClient();
 
 const meta: Meta = {
   title: 'Features/RequestNewGame/GenerateStoreIdentifierForm',
   tags: ['autodocs'],
   component: GenerateStoreIdentifierForm,
+  decorators: [
+    (Story) => {
+      return <QueryClientProvider client={queryClient}>
+        <GeneratedStoreIdentifierStoreProvider>
+          <Story />
+        </GeneratedStoreIdentifierStoreProvider>
+      </QueryClientProvider>;
+    },
+  ]
 };
 
 export default meta;
